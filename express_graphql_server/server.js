6b265f82dc4939c5afc55b07dcb82ef1bf8478b8
DavidWard98 <david.ae.ward@gmail.com>
2022-05-16 20:29:51 -0600

    feat: add rollDice to schema

---
+++ express_graphql_server/server.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var { graphqlHTTP } = require('express-graphql');
+var { buildSchema } = require('graphql');
+
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Query {
+    hello: String
+    rollDice(numSides: Int): Int
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+var root = {
+    hello: () => {
+        return 'Hello world!';
+    },
+    rollDice: (args) => {
+        return Math.floor(Math.random() * args.numSides) + 1;
+    }
+};
+
+var app = express();
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}));
+app.listen(4000);
+console.log('Running a GraphQL API server at http://localhost:4000/graphql');
